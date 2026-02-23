faea7e9e0fab92ad2dbd1a0e3d4ba8faf576c688
rupeshtadikamalla-design <rupeshtadikamalla@gmail.com>
2026-02-23 22:46:37 +0530

    UC4 using Array and Loops

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,11 +1,18 @@
 public class OOPSBanner {
-public static void main(String[] args) {
-System.out.println(String.join("", " OOO ", " OOO ", " PPPP ", " SSSS "));
-System.out.println(String.join("", " O O ", " O O ", " P P ", " S "));
-System.out.println(String.join("", " O O ", " O O ", " P P ", " S "));
-System.out.println(String.join("", " O O ", " O O ", " PPPP ", " SSS "));
-System.out.println(String.join("", " O O ", " O O ", " P ", " S "));
-System.out.println(String.join("", " O O ", " O O ", " P ", " S "));
-System.out.println(String.join("", " OOO ", " OOO ", " P ", " SSS "));
-}
+    public static void main(String[] args) {
+
+        // Array storing OOPS banner
+        String[] banner = {
+            "  OOO   OOO   PPPP   SSSS ",
+            " O   O O   O  P   P  S    ",
+            " O   O O   O  PPPP    SSS ",
+            " O   O O   O  P          S",
+            "  OOO   OOO   P      SSSS "
+        };
+
+        // Loop to print banner
+        for (int i = 0; i < banner.length; i++) {
+            System.out.println(banner[i]);
+        }
+    }
 }
